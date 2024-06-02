87dab138c4fdb02e7f47d472cb9eece5875ab47e
Gert-Jan de Boer <boerg@gertjandeboer.nl>
2024-06-02 13:05:13 +0200

    Fixed bodybatterylevel timestamps

--- garminha_bodybattery.py
+++ garminha_bodybattery.py
@@ -69,12 +69,14 @@ def bbvaluesjson_to_influxdb(host,port,database,json_data):
                     datasetEmpty = True
                 else:
                     datasetEmpty = False
+                    date = datetime.fromtimestamp(bodyBatteryValue[0])
+                    formatted_date = date.isoformat()
                     measurements.append({
                         "measurement": "bodyBattery",
                         "tags": {
                             "date": item['date'],
                         },
-                        "time": bodyBatteryValue[0]/1000,
+                        "time": formatted_date
                         "fields": {
                             "bodyBatteryLevel": bodyBatteryValue[1],
                         }
