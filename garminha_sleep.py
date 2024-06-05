c594682df03f4512f4da516b1c7bc0af7e7d087b
Gert-Jan de Boer <boerg@gertjandeboer.nl>
2024-06-05 11:13:27 +0200

    Fixed 0 values in Sleep

--- garminha_sleep.py
+++ garminha_sleep.py
@@ -32,38 +32,37 @@ def sleepjson_to_influxdb(host,port,database,json_data):
     if napTimeSeconds is not None:
         napTimeSeconds = int(napTimeSeconds)
     # Preparing json payload which will be pushed to InfluxDB
-    json_body = [{
-        "measurement": "dailySleepDTO",
-        "tags": {
-            "userID": json_data['dailySleepDTO'].get('userProfilePK', 0),
-            "sleepFromDevice": json_data['dailySleepDTO'].get('sleepFromDevice', 0),
-            "retro": json_data['dailySleepDTO'].get('retro', 0),
-            "sleppFromDevice": json_data['dailySleepDTO'].get('sleepFromDevice', 0),
-            "deviceRemCapable": json_data['dailySleepDTO'].get('deviceRemCapable', 0)
-        },
-        "time": json_data['dailySleepDTO'].get('calendarDate', 0),
-        "fields": {
-            if sleepTimeSeconds is not None:
+    if sleepTimeSeconds is not None and napTimeSeconds is not None:
+        json_body = [{
+            "measurement": "dailySleepDTO",
+            "tags": {
+                "userID": json_data['dailySleepDTO'].get('userProfilePK', 0),
+                "sleepFromDevice": json_data['dailySleepDTO'].get('sleepFromDevice', 0),
+                "retro": json_data['dailySleepDTO'].get('retro', 0),
+                "sleppFromDevice": json_data['dailySleepDTO'].get('sleepFromDevice', 0),
+                "deviceRemCapable": json_data['dailySleepDTO'].get('deviceRemCapable', 0)
+            },
+            "time": json_data['dailySleepDTO'].get('calendarDate', 0),
+            "fields": {
                 "sleepTimeSeconds": sleepTimeSeconds,
-            if napTimeSeconds is not None:
                 "napTimeSeconds": napTimeSeconds,
-            "autoSleepStartTimestampGMT": json_data['dailySleepDTO'].get('autoSleepStartTimestampGMT', 0),
-            "averageSpO2Value": float(json_data['dailySleepDTO'].get('averageSpO2Value', 0.0)),
-            "lowestSpO2Value": float(json_data['dailySleepDTO'].get('lowestSpO2Value', 0.0)),
-            "highestSpO2Value": float(json_data['dailySleepDTO'].get('highestSpO2Value', 0.0)),
-            "averageSpO2HRSleep": float(json_data['dailySleepDTO'].get('averageSpO2HRSleep', 0.0)),
-            "averageRespirationValue": float(json_data['dailySleepDTO'].get('averageRespirationValue', 0.0)),
-            "lowestRespirationValue": float(json_data['dailySleepDTO'].get('lowestRespirationValue', 0.0)),
-            "highestRespirationValue": float(json_data['dailySleepDTO'].get('highestRespirationValue', 0.0)),
-            "awakeCount": int(json_data['dailySleepDTO'].get('awakeCount', 0)),
-            "avgSleepStress": float(json_data['dailySleepDTO'].get('avgSleepStress', 0.0)),
-            "ageGroup": str(json_data['dailySleepDTO'].get('ageGroup', "Unknown")),
-            "sleepScoreFeedback": str(json_data['dailySleepDTO'].get('sleepScoreFeedback', "Unknown")),
-            "sleepScoreInsight": str(json_data['dailySleepDTO'].get('sleepScoreInsight', "Unknown")),
-            "sleepVersion": str(json_data['dailySleepDTO'].get('sleepVersion', "Unknown")),
-            "appType": "APP"
-        }
-    }]
+                "autoSleepStartTimestampGMT": json_data['dailySleepDTO'].get('autoSleepStartTimestampGMT', 0),
+                "averageSpO2Value": float(json_data['dailySleepDTO'].get('averageSpO2Value', 0.0)),
+                "lowestSpO2Value": float(json_data['dailySleepDTO'].get('lowestSpO2Value', 0.0)),
+                "highestSpO2Value": float(json_data['dailySleepDTO'].get('highestSpO2Value', 0.0)),
+                "averageSpO2HRSleep": float(json_data['dailySleepDTO'].get('averageSpO2HRSleep', 0.0)),
+                "averageRespirationValue": float(json_data['dailySleepDTO'].get('averageRespirationValue', 0.0)),
+                "lowestRespirationValue": float(json_data['dailySleepDTO'].get('lowestRespirationValue', 0.0)),
+                "highestRespirationValue": float(json_data['dailySleepDTO'].get('highestRespirationValue', 0.0)),
+                "awakeCount": int(json_data['dailySleepDTO'].get('awakeCount', 0)),
+                "avgSleepStress": float(json_data['dailySleepDTO'].get('avgSleepStress', 0.0)),
+                "ageGroup": str(json_data['dailySleepDTO'].get('ageGroup', "Unknown")),
+                "sleepScoreFeedback": str(json_data['dailySleepDTO'].get('sleepScoreFeedback', "Unknown")),
+                "sleepScoreInsight": str(json_data['dailySleepDTO'].get('sleepScoreInsight', "Unknown")),
+                "sleepVersion": str(json_data['dailySleepDTO'].get('sleepVersion', "Unknown")),
+                "appType": "APP"
+            }
+        }]
     # Write points to InfluxDB
     client.write_points(json_body)
 
